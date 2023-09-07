1121c140b0102ee6b07416989713a11ac87c6d89
Umair <umairwasim001@gmail.com>
2023-09-08 03:35:02 +0500

    Update Unit.cs

--- RTS-BeyondGames/Assets/Scripts/Unit/Unit.cs
+++ RTS-BeyondGames/Assets/Scripts/Unit/Unit.cs
@@ -6,13 +6,18 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMesh))]
 public class Unit : MonoBehaviour
 {
-    private NavMeshAgent agent;
+    private const string IS_WALKING = "IsWalking";
+
     [SerializeField] private SpriteRenderer selectionSprite;
+
+    private NavMeshAgent agent;
+    private Animator animator;
 
     private void Awake()
     {
         Init();
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     void Init()
@@ -34,5 +39,14 @@ public class Unit : MonoBehaviour
     public void SetDestinationPoint(Vector3 destination)
     {
         agent.SetDestination(destination);
+    }
+
+    public void StartAnimation()
+    {
+        Debug.Log("agent.velocity.magnitude " + agent.velocity.magnitude);
+        if (!agent.isStopped)
+            animator.SetBool(IS_WALKING, true);
+        else
+            animator.SetBool(IS_WALKING, false);
     }
 }
