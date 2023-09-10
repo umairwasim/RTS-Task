e251f3a9be7619163280b9d780eec4cf2ab9eaf1
Umair <umairwasim001@gmail.com>
2023-09-11 03:17:06 +0500

    Update TeleportManager.cs
    For Testung

--- RTS-BeyondGames/Assets/Scripts/Managers/TeleportManager.cs
+++ RTS-BeyondGames/Assets/Scripts/Managers/TeleportManager.cs
@@ -2,32 +2,59 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class TeleportManager : MonoBehaviour
 {
     public static TeleportManager Instance;
 
-    private HashSet<Teleporter> teleporterList = new();
+    private List<Teleporter> teleporterList = new();
+
+    //for testing only
+    public GameObject player;
+    public GameObject teleporterPrefab;
+
+    private bool isEntered = false;
 
     private void Awake()
     {
         Instance = this;
     }
 
+    private void Start()
+    {
+        StartCoroutine(InstantiateCube());
+    }
+
+    IEnumerator InstantiateCube()
+    {
+        Instantiate(teleporterPrefab, player.transform.position + new Vector3(5, 0, 0), Quaternion.identity);
+        yield return new WaitForSeconds(3f);
+        Instantiate(teleporterPrefab, player.transform.position + new Vector3(0, 0, 5), Quaternion.identity);
+    }
+
     public void AddCube(Teleporter teleporter)
     {
         teleporterList.Add(teleporter);
+        Debug.Log("Teleporter added" + teleporter);
     }
+
+    //private Teleporter OtherTeleporter(Teleporter teleporter)
+    //{
+    //    return teleporterList.Where(t => t.IsPlayerEntered() == teleporter.IsPlayerEntered());
+    //}
 
     public void Teleport()
     {
         foreach (var teleporter in teleporterList)
         {
             //check if the player entered in the teleporter
-            if(teleporter.IsPlayerEntered())
+            if (teleporter.IsPlayerEntered())
             {
-                
+                Transform teleportToTransform = teleporterList[1].teleporterPointTransform;
+                player.transform.position = teleportToTransform.position;
+                player.GetComponent<Unit>().SetDestinationPoint(teleportToTransform.position);
             }
         }
-    }    
+    }
 }
